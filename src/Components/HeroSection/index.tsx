421bd571ac2e9d675f593ee00e0fd4e052577275
selly361 <seikou615@gmail.com>
2023-12-04 19:45:08 +0000

    Add: `HeroSection` component

---
+++ src/Components/HeroSection/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <section className='hero-section'>
+      <article className='hero-section__article'>
+        <h1>Next generation digital banking</h1>
+        <p>
+          Take your financial life online. Your Easybank account will be a
+          one-stop-shop for spending, saving, budgeting, investing, and much
+          more.
+        </p>
+        <button className="button hero-section__article__button">Request Invite</button>
+      </article>
+      <img className='hero-section__image' src='images/image-mockups.png' alt='' />
+    </section>
+  )
+}
+
+export default HeroSection
