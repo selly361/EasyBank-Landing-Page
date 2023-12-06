31ca174a1093a1de889ff9c32355799cad9b606c
selly361 <seikou615@gmail.com>
2023-12-06 20:59:38 +0000

    Update: Exported `Articles` component

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header"
 export { default as HeroSection } from "./HeroSection"
 export { default as KeyFeatures } from "./KeyFeatures"
+export { default as Articles } from "./Articles"
